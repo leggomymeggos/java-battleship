09fe7fe8dea2fc482b67bc7108fb29f3b669abe5
Megan Gaebler <megan.gaebler@gmail.com>
2018-03-25 21:17:39 -0500

    Add some kind of board; still need to get board

--- frontend/src/components/board/Board.tsx
+++ frontend/src/components/board/Board.tsx
@@ -1,17 +1,61 @@
 import * as React from "react";
 
-export class Board extends React.Component {
+export interface IBoardPropsFromActions {
+    actions: {
+        getInitialBoard: any;
+    };
+}
+
+export interface IBoardPropsFromStore {
+    coordinates: any[][];
+}
+
+export type BoardProps = IBoardPropsFromActions & IBoardPropsFromStore;
+
+export class Board extends React.Component<BoardProps> {
+    private alphabet = [...Array(26)]
+        .map((q, w) => String.fromCharCode(w + 97));
+
+    public componentWillMount() {
+        this.props.actions.getInitialBoard();
+    }
+
     public render() {
         return <div>
-            Hey bro I'm the board bro
+            {this.renderRowLabels()}
+            {this.renderColumnLabels()}
+            {this.renderGrid()}
+        </div>
+    }
+
+    private renderRowLabels() {
+        return <div className="board__labels--row">{
+            this.props.coordinates
+                .map((it, index) => {
+                    return <div className="board__label" key={index}>
+                        {index + 1}
+                    </div>;
+                })
+        }</div>;
+    }
+
+    private renderColumnLabels() {
+        return <div className="board__labels--column">
+            {this.props.coordinates[0].map((_, index) => {
+                return <div key={index}>
+                    {this.alphabet[index].toUpperCase()}
+                </div>
+            })}
         </div>;
     }
 
-    private renderTile() {
-        return <div className="batttleship-tile" />
-    }
-
     private renderGrid() {
-        return
+        return <div className="board__grid">
+            {this.props.coordinates
+                .reduce((a, b) => a.concat(b), [])
+                .map((_, index) => {
+                    return <div className="board__tile" key={index}/>
+                })}
+        </div>
     }
 }
--- frontend/src/components/board/__tests__/Board.test.tsx
+++ frontend/src/components/board/__tests__/Board.test.tsx
@@ -1,7 +1,69 @@
+import {shallow} from "enzyme";
 import * as React from "react";
+import {Board, BoardProps} from "../Board";
 
 describe("Board", () => {
-    it("can do stuff", () => {
-        expect(true).toBeTruthy();
+    let defaultProps: BoardProps;
+    let mockActions: any;
+
+    beforeEach(() => {
+        mockActions = {
+            getInitialBoard: jest.fn()
+        };
+        defaultProps = {
+            coordinates: [[]],
+            actions: mockActions,
+        }
+    });
+
+    it("has a grid", () => {
+        const subject = shallow(<Board {...defaultProps} />);
+
+        expect(subject.find(".board__grid").exists()).toBeTruthy();
+    });
+
+    it("has a tile for each coordinate", () => {
+        const props  = {
+            ...defaultProps,
+            coordinates: [[1, 2], [3, 4]],
+        };
+        const subject = shallow(<Board {...props} />);
+
+        const tiles = subject.find(".board__tile");
+
+        expect(tiles).toHaveLength(4);
+    });
+
+    it("has coordinate row labels", () => {
+        const props  = {
+            ...defaultProps,
+            coordinates: [[1, 2], [3, 4], [5, 6]],
+        };
+        const subject = shallow(<Board {...props} />);
+
+        const rowLabels = subject.find(".board__labels--row");
+
+        expect(rowLabels.text()).toContain("1");
+        expect(rowLabels.text()).toContain("2");
+        expect(rowLabels.text()).toContain("3");
+    });
+
+    it("has coordinate column labels", () => {
+        const props  = {
+            ...defaultProps,
+            coordinates: [[1, 2], [3, 4], [5, 6]],
+        };
+        const subject = shallow(<Board {...props} />);
+
+        const columnLabels = subject.find(".board__labels--column");
+
+        expect(columnLabels.text()).toContain("A");
+        expect(columnLabels.text()).toContain("B");
+    });
+
+    it("gets a new board from the backend", () => {
+        shallow(<Board {...defaultProps}/>);
+
+        expect(mockActions.getInitialBoard).toHaveBeenCalled();
     });
 });
