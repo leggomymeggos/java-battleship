823cc835393b1e153f62991bb43be56c8d4c44f1
Megan Gaebler <megan.gaebler@gmail.com>
2018-09-25 10:16:38 -0700

    make tests pass

--- frontend/src/components/board/__tests__/boardReducer.test.ts
+++ frontend/src/components/board/__tests__/boardReducer.test.ts
@@ -11,6 +11,17 @@ describe("board reducer", () => {
     it("handles GET_INITIAL_BOARD action", () => {
         const state = boardReducer(initialState, {type: BoardActions.GET_INITIAL_BOARD});
 
-        expect(state.coordinates).toEqual([[1, 2, 3], [1, 2, 3], [1, 2, 3]]);
+        expect(state.coordinates).toEqual([
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        ]);
     });
 });
