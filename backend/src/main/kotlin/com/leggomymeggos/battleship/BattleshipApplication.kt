1f41257a381066f4f6d73fb94b06593a90fd93fb
Megan Gaebler <megan.gaebler@gmail.com>
2018-09-27 15:10:42 -0700

    real backend!

---
+++ backend/src/main/kotlin/com/leggomymeggos/battleship/BattleshipApplication.kt
@@ -0,0 +1,11 @@
+package com.leggomymeggos.battleship
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BattleshipApplication
+
+fun main(args: Array<String>) {
+    runApplication<BattleshipApplication>(*args)
+}
