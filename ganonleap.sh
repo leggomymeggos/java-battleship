4be3133b34c4889689ad24fa573450d5e4f08777
Megan Gaebler <megan.gaebler@gmail.com>
2018-10-04 22:08:59 -0700

    changing up pre-push stuff; now ganon flies across the screen on test error

---
+++ ganonleap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+unicornleap -u ~/workspace/java-battleship/test_images/ganon.png -k ~/workspace/java-battleship/test_images/rupee.png
---
+++ pre-push.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+pushd ~/workspace/java-battleship;
+
+cd backend && ./gradlew clean build &&
+cd ../frontend && npm test;
+
+if [ $? == 0 ]; then
+    popd && gosleap
+else
+    popd && ./ganonleap.sh
+fi
+
+
