ced97c7b5f6e3d5b08c64fba66144d6377e3ba69
Megan Gaebler <megan.gaebler@gmail.com>
2019-01-02 12:40:00 -0800

    Bowing to bash prescriptivism

--- pre-push.sh
+++ pre-push.sh
@@ -5,7 +5,7 @@ pushd ~/workspace/java-battleship;
 cd backend && ./gradlew clean build &&
 cd ../frontend && npm test;
 
-if [ $? == 0 ]; then
+if [[ $? == 0 ]]; then
     popd && gosleap
 else
     popd && ./ganonleap.sh -e 2
