ae1dac353eb505f0ad2b7d899e115427d86a75c3
Megan Gaebler <megan.gaebler@gmail.com>
2019-01-30 13:01:46 -0800

    ouTpUtS

--- concourse/backend/build-dev/run.sh
+++ concourse/backend/build-dev/run.sh
@@ -2,7 +2,7 @@
 
 cd source-code/backend && ./gradlew clean build -x test &&
 
-cd .. &&
+cd ../.. &&
 
 mv source-code/backend build-output/backend &&
 mv source-code/manifests build-output/manifests
